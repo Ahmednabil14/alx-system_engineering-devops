7aa6e9dfc7e88205d55a005de177c1c7d9d95618
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-05 16:03:23 +0200

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+# Add a custom HTTP header with Puppet
+exec {'update packages':
+    command => ' sudo apt-get update',
+    path    => ['/usr/bin', '/bin'],
+}
+package {'haproxy':
+    ensure => 'present',
+}
+exec {'append X-Served-By header':
+    command => 'sudo sed -i "/listen \[::\]:80 default_server/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
+    path    => ['/usr/bin', '/bin'],
+}
+exec {'restart haproxy':
+    command => 'sudo service nginx restart',
+    path    => ['/usr/bin', '/bin'],
+}
