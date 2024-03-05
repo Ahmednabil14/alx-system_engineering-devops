b10f19f802cd0a17edfbfc0bb134d5b0451fbd4e
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-05 16:48:22 +0200

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Add a custom HTTP header with Puppet
 exec {'update packages':
-    command => ' sudo apt-get update',
+    command => 'sudo apt-get update',
     path    => ['/usr/bin', '/bin'],
 }
 package {'nginx':
@@ -10,7 +10,7 @@ exec {'append X-Served-By header':
     command => 'sudo sed -i "/listen \[::\]:80 default_server/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
     path    => ['/usr/bin', '/bin'],
 }
-exec {'restart nginx':
+exec {'restart nginx' :
     command => 'sudo service nginx restart',
-    path    => ['/usr/bin', '/bin'],
+    path    => ['/usr/sbin', '/usr/bin'],
 }
