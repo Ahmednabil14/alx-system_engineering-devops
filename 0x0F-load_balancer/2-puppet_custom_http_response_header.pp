74f934a40f50d8537db8a99c4d87029135d4237f
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-05 16:17:36 +0200

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,14 +3,14 @@ exec {'update packages':
     command => ' sudo apt-get update',
     path    => ['/usr/bin', '/bin'],
 }
-package {'haproxy':
+package {'nginx':
     ensure => 'present',
 }
 exec {'append X-Served-By header':
     command => 'sudo sed -i "/listen \[::\]:80 default_server/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
     path    => ['/usr/bin', '/bin'],
 }
-exec {'restart haproxy':
+exec {'restart nginx':
     command => 'sudo service nginx restart',
     path    => ['/usr/bin', '/bin'],
 }
