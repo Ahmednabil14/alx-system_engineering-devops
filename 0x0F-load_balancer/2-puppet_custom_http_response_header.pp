3dfcdb1ab359196f5bb072af9819a8a5aaffe7cc
Ahmednabil14 <ahmednabil14499@gmail.com>
2024-03-05 17:19:39 +0200

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,9 +6,13 @@ exec {'update packages':
 package {'nginx':
     ensure => 'present',
 }
-exec {'append X-Served-By header':
-    command => 'sudo sed -i "/listen \[::\]:80 default_server/a add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
-    path    => ['/usr/bin', '/bin'],
+file_line {'add X-Served-By header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname;',
+  # line i will add my line after
+  match   => 'listen \[::\]:80 default_server',
+  require => Package['nginx'],
 }
 exec {'restart nginx' :
     command => 'sudo service nginx restart',
